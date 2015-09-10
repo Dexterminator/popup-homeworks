ad3a94c146715920ec5fcb0914b8023a644cf676
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-09-10 15:16:48 +0200

    Slightly faster help.

--- week1/help/src/se/dxtr/Main.java
+++ week1/help/src/se/dxtr/Main.java
@@ -1,22 +1,26 @@
 package se.dxtr;
 
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Main {
 
     static Map<String, String> mappings1 = new HashMap<> ();
     static Map<String, String> mappings2 = new HashMap<> ();
+    private static Kattio io;
 
     public static void main(String[] args) {
-        Scanner s = new Scanner (System.in);
+        Scanner s = new Scanner (new BufferedReader (new InputStreamReader (System.in)));
+        io = new Kattio (System.in, System.out);
         int n = s.nextInt ();
         s.nextLine ();
         for (int i = 0; i < n; i++)
             matchPatterns (s);
+        io.flush ();
+        io.close ();
     }
 
     private static void matchPatterns (Scanner s) {
@@ -24,33 +28,49 @@ public class Main {
         String s2 = s.nextLine ();
         String[] tokens1 = s1.split (" ");
         String[] tokens2 = s2.split (" ");
-        System.out.println (findMatch (tokens1, tokens2));
+        for (String str : findMatch (tokens1, tokens2)) {
+            io.print (str + " ");
+        }
+        io.println ();
     }
 
-    private static String findMatch (String[] tokens1, String[] tokens2) {
+    private static String[] findMatch (String[] tokens1, String[] tokens2) {
         if (tokens1.length != tokens2.length)
-            return "-";
+            return new String[]{"-"};
 
         mappings1.clear ();
         mappings2.clear ();
 
-        createExplicitMappings (tokens1, tokens2);
+        boolean createdWithoutContradictions = createExplicitMappings (tokens1, tokens2);
+        if (!createdWithoutContradictions)
+            return new String[]{"-"};
         resolveOtherMappings (tokens1, tokens2);
 
         return getResult (tokens1, tokens2);
     }
 
-    private static void createExplicitMappings (String[] tokens1, String[] tokens2) {
+    private static boolean createExplicitMappings (String[] tokens1, String[] tokens2) {
         for (int j = 0; j < tokens1.length; j++) {
             String token1 = tokens1[j];
             String token2 = tokens2[j];
 
             if (isPlaceholder (token1) && !isPlaceholder (token2)) {
-                mappings1.putIfAbsent (token1, token2);
+                if (mappings1.containsKey (token1)) {
+                    if (!mappings1.get (token1).equals (token2))
+                        return false;
+                } else {
+                    mappings1.put (token1, token2);
+                }
             } else if (!isPlaceholder (token1) && isPlaceholder (token2)) {
-                mappings2.putIfAbsent (token2, token1);
+                if (mappings2.containsKey (token2)) {
+                    if (!mappings2.get (token2).equals (token1))
+                        return false;
+                } else {
+                    mappings2.put (token2, token1);
+                }
             }
         }
+        return true;
     }
 
     private static void resolveOtherMappings (String[] tokens1, String[] tokens2) {
@@ -68,7 +88,7 @@ public class Main {
         }
     }
 
-    private static String getResult (String[] tokens1, String[] tokens2) {
+    private static String[] getResult (String[] tokens1, String[] tokens2) {
         String[] result = new String[tokens1.length];
         for (int j = 0; j < tokens1.length; j++) {
             String token1 = tokens1[j];
@@ -78,12 +98,12 @@ public class Main {
             String string2 = isPlaceholder (token2) ? mappings2.getOrDefault (token2, "a") : token2;
 
             if (!string1.equals (string2))
-                return "-";
+                return new String[] {"-"};
 
             result[j] = string1;
         }
 
-        return Arrays.stream (result).collect (Collectors.joining (" "));
+        return result;
     }
 
     private static boolean isPlaceholder (String token1) {
