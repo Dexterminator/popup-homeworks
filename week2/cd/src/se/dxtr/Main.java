bd39cd779f243950cb1381f204a65ba9aad5a8b9
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-09-16 15:46:24 +0200

    Cd accepted on kattis.

--- week2/cd/src/se/dxtr/Main.java
+++ week2/cd/src/se/dxtr/Main.java
@@ -4,15 +4,60 @@ public class Main {
 
     public static void main(String[] args) {
         Kattio io = new Kattio (System.in, System.out);
-        int jacksCds = io.getInt ();
-        int jillsCds = io.getInt ();
+        while (io.hasMoreTokens ()) {
+            solve (io);
+        }
+        io.close ();
+    }
 
-        for (int i = 0; i < jacksCds; i++) {
+    private static void solve (Kattio io) {
+        int jacksNumberOfCds = io.getInt ();
+        int jillsNumberOfCds = io.getInt ();
 
+        if (jacksNumberOfCds == 0 && jillsNumberOfCds == 0) {
+            return;
+        } else if (jacksNumberOfCds == 0) {
+            for (int i = 0; i < jillsNumberOfCds; i++)
+                io.getInt ();
+            io.println (0);
+            return;
+        } else if (jillsNumberOfCds == 0) {
+            for (int i = 0; i < jacksNumberOfCds; i++)
+                io.getInt ();
+            io.println (0);
+            return;
         }
 
-        for (int i = 0; i < jillsCds; i++) {
+        int numberOfCdsOwnedByBoth = 0;
+        int[] jacksCds = new int[jacksNumberOfCds];
 
+        for (int i = 0; i < jacksNumberOfCds; i++)
+            jacksCds[i] = io.getInt ();
+
+        int jackIndex = 0;
+        int jillsCd = io.getInt ();
+        int jillIndex = 0;
+        while (jillIndex < jillsNumberOfCds && jackIndex < jacksNumberOfCds) {
+            if (jillsCd == jacksCds[jackIndex]) {
+                numberOfCdsOwnedByBoth++;
+                if (jillIndex < jillsNumberOfCds - 1)
+                    jillsCd = io.getInt ();
+                jillIndex++;
+                jackIndex++;
+            } else if (jillsCd > jacksCds[jackIndex]) {
+                jackIndex++;
+            } else { // jillsCd < jacksCds[jackIndex]
+                if (jillIndex < jillsNumberOfCds - 1)
+                    jillsCd = io.getInt ();
+                jillIndex++;
+            }
         }
+        
+        while (jillIndex < jillsNumberOfCds - 1) {
+            io.getInt ();
+            jillIndex++;
+        }
+
+        io.println (numberOfCdsOwnedByBoth);
     }
 }
