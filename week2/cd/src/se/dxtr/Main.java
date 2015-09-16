0c9fccb64da1939fff9705d6f4c0335da53907b7
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-09-16 08:28:27 +0200

    cd skeleton.

---
+++ week2/cd/src/se/dxtr/Main.java
@@ -0,0 +1,18 @@
+package se.dxtr;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Kattio io = new Kattio (System.in, System.out);
+        int jacksCds = io.getInt ();
+        int jillsCds = io.getInt ();
+
+        for (int i = 0; i < jacksCds; i++) {
+
+        }
+
+        for (int i = 0; i < jillsCds; i++) {
+
+        }
+    }
+}
