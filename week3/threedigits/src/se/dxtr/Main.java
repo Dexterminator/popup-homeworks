e4e4609dd5085f90a5d249efe5f485742bb4f21e
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-09-25 10:14:45 +0200

    Another try for three digits.

--- week3/threedigits/src/se/dxtr/Main.java
+++ week3/threedigits/src/se/dxtr/Main.java
@@ -5,7 +5,8 @@ public class Main {
 
     public static void main (String[] args) {
         io = new Kattio (System.in, System.out);
-        int n = io.getInt ();
+//        int n = io.getInt ();
+        int n = 10000000-100;
         String threeDigits = threeDigits (n);
         io.println (threeDigits);
         io.close ();
@@ -13,27 +14,41 @@ public class Main {
 
     private static String threeDigits (int n) {
         long factorial = 1;
+        int signifcance = 0;
         for (int i = 2; i <= n; i++) {
-            factorial *= i;
+            String truncatedI = removeTrailingZeroes (i);
+            factorial *= Integer.valueOf (truncatedI);
             String truncated = removeTrailingZeroes (factorial);
-            int toKeep = truncated.length () > 3 ? getToKeep (truncated) : truncated.length ();
-            String substring = truncated.substring (truncated.length () - toKeep, truncated.length ());
+//            int toKeep = truncated.length () > 3 ? getToKeep (truncated) : truncated.length ();
+//            String substring = truncated.substring (truncated.length () - toKeep, truncated.length ());
+            int newSignificane = truncated.length () - 1;
+            if (newSignificane > signifcance)
+                signifcance = newSignificane;
+            String substring = truncated.length () <= 3 ? truncated : truncated.substring (truncated.length () - 3, truncated.length ());
+//            System.err.println ("truncated: " + truncated);
+//            System.err.println ("substring: " + substring);
             factorial = Long.valueOf (substring);
+//            System.err.println ("factorial after substring: " + factorial);
         }
-        String s = String.valueOf (factorial);
-        return s.length () <= 3 ? s : s.substring (s.length () - 3, s.length ());
-    }
 
-    private static int getToKeep (String truncated) {
-        int i;
-        char[] digits = truncated.toCharArray ();
-        i = digits.length - 2; // Last digit cannot be zero
-        int toKeep = 1;
-        while (digits[i] == '0' || toKeep < 3) {
-            toKeep++;
-            i--;
+        String s = String.valueOf (factorial);
+        if (s.length () == 1) {
+            if (signifcance > 2)
+                return "00" + s;
+            if (signifcance == 1)
+                return "0" + s;
+            return s;
         }
-        return toKeep + 1;
+
+        if (s.length () == 2) {
+            if (signifcance > 1)
+                return "0" + s;
+            return s;
+        }
+
+        if (s.length () > 3)
+            return s.substring (s.length () - 3, s.length ());
+        return s;
     }
 
     private static String removeTrailingZeroes (long factorial) {
