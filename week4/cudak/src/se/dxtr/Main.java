efb0c9de5f856d0bc45d36edb1cd51f7aca01e29
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-09-30 18:42:57 +0200

    Cudak skeleton.

---
+++ week4/cudak/src/se/dxtr/Main.java
@@ -0,0 +1,12 @@
+package se.dxtr;
+
+public class Main {
+    static Kattio io = new Kattio (System.in, System.out);
+
+    public static void main(String[] args) {
+        int left = io.getInt ();
+        int right = io.getInt ();
+        int digitSum = io.getInt ();
+        
+    }
+}
