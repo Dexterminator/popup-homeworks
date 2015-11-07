ce220fb070892a3c5f93371e705afbc6149905ef
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-11-07 17:11:19 +0100

    Sat accepted on kattis.

--- week7/src/se/dxtr/satisfiability/Satisfiability.java
+++ week7/src/se/dxtr/satisfiability/Satisfiability.java
@@ -1,6 +1,8 @@
 package se.dxtr.satisfiability;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -19,6 +21,10 @@ public class Satisfiability {
             this.variableValue = variableValue;
         }
 
+        public boolean getBooleanValue(boolean assignedValue) {
+            return variableValue.apply(assignedValue);
+        }
+
         @Override
         public String toString() {
             return "Variable{" +
@@ -30,85 +36,94 @@ public class Satisfiability {
 
     static Function<Boolean, Boolean> not = b -> !b;
     static Kattio io = new Kattio(System.in, System.out);
-    static boolean satisfiable;
 
     public static void main(String[] args) throws IOException {
         int cases = io.getInt();
-        for (int i = 0; i < cases; i++) {
-            handleCase();
-        }
+        for (int i = 0; i < cases; i++)
+            solveInstance();
         io.close();
     }
 
-    private static void handleCase() throws IOException {
-//                    System.err.println("case " + i);
+    private static void solveInstance() throws IOException {
         int variablesCount = io.getInt();
         int clauseCount = io.getInt();
+        List<Variable[]> clauses = getClauses(clauseCount);
+        new Instance(clauses, variablesCount).solve();
+    }
+
+    private static List<Variable[]> getClauses(int clauseCount) throws IOException {
         List<Variable[]> clauses = new ArrayList<>();
         for (int j = 0; j < clauseCount; j++) {
             String line = io.getLine();
-            String[] split = line.split(" v ");
-            Variable[] clause = new Variable[split.length];
-            parseClause(split, clause);
+            Variable[] clause = parseClause(line);
             clauses.add(clause);
         }
-        satisfiable = false;
-        solve(variablesCount, clauses);
+        return clauses;
     }
 
-    private static void parseClause(String[] split, Variable[] clause) {
+    private static Variable[] parseClause(String line) {
+        String[] split = line.split(" v ");
+        Variable[] clause = new Variable[split.length];
         for (int k = 0; k < split.length; k++) {
             String s = split[k];
             if (s.startsWith("~")) {
-                clause[k] = new Variable(Integer.parseInt(s.substring(2, 3)) - 1, not);
+                clause[k] = new Variable(Integer.parseInt(s.substring(2, s.length())) - 1, not);
             } else {
-                clause[k] = new Variable(Integer.parseInt(s.substring(1, 2)) - 1, Function.identity());
+                clause[k] = new Variable(Integer.parseInt(s.substring(1, s.length())) - 1, Function.identity());
             }
         }
+        return clause;
     }
 
-    private static void solve(int variablesCount, List<Variable[]> clauses) {
-        boolean[] assignment = new boolean[variablesCount];
-        testAssignment(0, clauses, assignment);
-        if (satisfiable) {
-            io.println("satisfiable");
-        } else {
-            io.println("unsatisfiable");
+    static class Instance {
+        private boolean satisfiable = false;
+        private final List<Variable[]> clauses;
+        private final boolean[] assignment;
+
+        public Instance(List<Variable[]> clauses, int variablesCount) {
+            this.clauses = clauses;
+            this.assignment = new boolean[variablesCount];
         }
-    }
 
-    private static void testAssignment(int k, List<Variable[]> clauses, boolean[] assignment) {
-        if (satisfiable)
-            return;
-
-        if (k == assignment.length) {
-            System.err.println("");
-            System.err.println("assignment = " + Arrays.toString(assignment));
-            for (Variable[] clause : clauses) {
-                boolean satisfied = isSatisfied(assignment, clause);
-                System.err.println("satisfied = " + satisfied);
-                if (!satisfied)
-                    return;
+        public void solve() {
+            testAssignment(0);
+            if (satisfiable) {
+                io.println("satisfiable");
+            } else {
+                io.println("unsatisfiable");
             }
-            // All clauses are satisfied
-            satisfiable = true;
-            return;
         }
 
-        testAssignment(k + 1, clauses, assignment);
-        assignment[k] = true;
-        testAssignment(k + 1, clauses, assignment);
-        assignment[k] = false;
-    }
+        private void testAssignment(int k) {
+            if (satisfiable)
+                return;
 
-    private static boolean isSatisfied(boolean[] assignment, Variable[] clause) {
-        System.err.println("clause = " + Arrays.toString(clause));
-        for (Variable variable : clause) {
-            boolean assignedValue = assignment[variable.id];
-            if (variable.variableValue.apply(assignedValue))
-                return true;
+            if (k == assignment.length) {
+                for (Variable[] clause : clauses) {
+                    boolean satisfied = isSatisfied(clause);
+                    if (!satisfied)
+                        return;
+                }
+                // All clauses are satisfied
+                satisfiable = true;
+                return;
+            }
+
+            testAssignment(k + 1);
+            assignment[k] = true;
+            testAssignment(k + 1);
+            assignment[k] = false;
         }
-        // No variable was true
-        return false;
+
+        private boolean isSatisfied(Variable[] clause) {
+            for (Variable variable : clause) {
+                boolean assignedValue = assignment[variable.id];
+                if (variable.getBooleanValue(assignedValue)) {
+                    return true;
+                }
+            }
+            // No variable was true, not satisfied
+            return false;
+        }
     }
 }
