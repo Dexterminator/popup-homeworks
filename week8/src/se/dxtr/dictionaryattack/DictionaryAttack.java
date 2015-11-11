ec8712e78e4966604073365a0d2043cfd991591e
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-11-11 21:48:05 +0100

    Another try.

--- week8/src/se/dxtr/dictionaryattack/DictionaryAttack.java
+++ week8/src/se/dxtr/dictionaryattack/DictionaryAttack.java
@@ -1,5 +1,7 @@
 package se.dxtr.dictionaryattack;
 
+import java.util.Arrays;
+
 /**
  * Created by dexter on 11/11/15.
  */
@@ -31,7 +33,7 @@ public class DictionaryAttack {
         return true;
     }
 
-    static int partDist(String pw, String dw, int pwLen, int dwLen) {
+    static int partDist(char[] pw, char[] dw, int pwLen, int dwLen) {
         if (pwLen == 0)
             return dwLen;
         if (dwLen == 0)
@@ -41,15 +43,12 @@ public class DictionaryAttack {
         return Math.min(exchangeDigitDistance, transposeDistance);
     }
 
-    private static int getTransposeDistance(String pw, String dw, int pwLen, int dwLen) {
+    private static int getTransposeDistance(char[] pw, char[] dw, int pwLen, int dwLen) {
         int transposeDistance = Integer.MAX_VALUE;
-        if (pwLen > 1 && dwLen > 1 && pw.charAt(pwLen - 1) == dw.charAt(dwLen - 2) && pw.charAt(pwLen - 2) == dw.charAt(dwLen - 1)) {
-            transposeDistance = partDist(pw, dw, pwLen - 2, dwLen - 2);
-            if (transposeDistance != Integer.MAX_VALUE)
-                transposeDistance += 1;
-        } else if (pwLen > 1) {
-            String swapped = swap(pw, pwLen - 1, pwLen - 2);
-            transposeDistance = partDist(swapped, dw, pwLen - 1, dwLen - 1);
+        if (pwLen > 1) {
+            char[] pwCpy = Arrays.copyOf(pw, pw.length);
+            swap(pwCpy, pwLen - 1, pwLen - 2);
+            transposeDistance = partDist(pwCpy, dw, pwLen - 1, dwLen - 1);
             if (transposeDistance != Integer.MAX_VALUE)
                 transposeDistance += 1;
         }
@@ -57,18 +56,17 @@ public class DictionaryAttack {
         return transposeDistance;
     }
 
-    private static String swap(String pw, int i, int j) {
-        char[] chars = pw.toCharArray();
-        char tmp = chars[i];
-        chars[i] = chars[j];
-        chars[j] = tmp;
-        return String.valueOf(chars);
+    private static String swap(char[] pw, int i, int j) {
+        char tmp = pw[i];
+        pw[i] = pw[j];
+        pw[j] = tmp;
+        return String.valueOf(pw);
     }
 
-    private static int getExchangeDigitDistance(String pw, String dw, int w1len, int w2len) {
+    private static int getExchangeDigitDistance(char[] pw, char[] dw, int w1len, int w2len) {
         int digitExchangeCost = Integer.MAX_VALUE;
-        char pwChar = pw.charAt(w1len - 1);
-        char dwChar = dw.charAt(w2len - 1);
+        char pwChar = pw[w1len - 1];
+        char dwChar = dw[w2len - 1];
         if (pwChar == dwChar) {
             digitExchangeCost = 0;
         } else if (Character.isDigit(pwChar)) {
@@ -86,7 +84,9 @@ public class DictionaryAttack {
     }
 
     static int distance(String w1, String w2) {
-        return partDist(w1, w2, w1.length(), w2.length());
+        if (w1.length() != w2.length())
+            return Integer.MAX_VALUE;
+        return partDist(w1.toCharArray(), w2.toCharArray(), w1.length(), w2.length());
     }
 
 }
