a105da3d318d7b529b108e96cbde361664408e3d
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-11-14 23:47:52 +0100

    Evil straw accepted.

--- week8/src/se/dxtr/evilstraw/EvilStraw.java
+++ week8/src/se/dxtr/evilstraw/EvilStraw.java
@@ -1,11 +1,9 @@
 package se.dxtr.evilstraw;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by dexter on 13/11/15.
- */
 public class EvilStraw {
 
     static Kattio io = new Kattio();
@@ -27,7 +25,7 @@ public class EvilStraw {
         Map<Character, Integer> letterCounts = new HashMap<>();
         for (int i = 0; i < word.length; i++)
             letterCounts.merge(word[i], 1, (a, b) -> a + b);
-        
+
         if (word.length % 2 == 0)
             return transformationPossibleEvenLength(letterCounts);
         return transformationPossibleOddLength(letterCounts);
@@ -53,18 +51,50 @@ public class EvilStraw {
     }
 
     private static int countSwaps(char[] word) {
-        return -1;
+        return countSwaps(word, 0, 0, word.length - 1);
     }
 
-    public static boolean isPalindrome(char[] word) {
-        int i = 0;
-        int j = word.length - 1;
-        while (j > i) {
-            if (word[i] != word[j])
-                return false;
-            i++;
-            j--;
+    private static int countSwaps(char[] word, int swaps, int swapStart, int swapEnd) {
+        if (swapStart > swapEnd)
+            return swaps;
+        if (word[swapStart] == word[swapEnd])
+            return countSwaps(word, swaps, swapStart + 1, swapEnd - 1);
+
+        int fewestSwaps = Integer.MAX_VALUE;
+        char[] bestWord = null;
+        for (int i = swapStart; i < swapEnd; i++) {
+            char[] swapWord = Arrays.copyOf(word, word.length);
+            char toSwap = swapWord[i];
+            for (int j = i; j < swapEnd; j++) {
+                swapWord[j] = swapWord[j + 1];
+            }
+            swapWord[swapEnd] = toSwap;
+            if (swapWord[swapStart] == swapWord[swapEnd]) {
+                int currSwaps = swapEnd - i;
+                if (currSwaps < fewestSwaps) {
+                    fewestSwaps = currSwaps;
+                    bestWord = swapWord;
+                }
+            }
         }
-        return true;
+
+        for (int i = swapEnd; i > swapStart; i--) {
+            char[] swapWord = Arrays.copyOf(word, word.length);
+            char toSwap = swapWord[i];
+            for (int j = i - 1; j >= swapStart; j--) {
+                swapWord[j + 1] = swapWord[j];
+            }
+
+            swapWord[swapStart] = toSwap;
+            if (swapWord[swapStart] == swapWord[swapEnd]) {
+                int currSwaps = i - swapStart;
+                if (currSwaps < fewestSwaps) {
+                    fewestSwaps = currSwaps;
+                    bestWord = swapWord;
+                }
+            }
+        }
+
+        return countSwaps(bestWord, swaps + fewestSwaps, swapStart + 1, swapEnd - 1);
     }
 }
