3e99d573ea1e9d999159aeffb7d16d7b505782b7
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-11-25 22:02:05 +0100

    Stuff.

--- week10/src/se/dxtr/howmanyzeros/HowManyZeros.java
+++ week10/src/se/dxtr/howmanyzeros/HowManyZeros.java
@@ -4,6 +4,18 @@ public class HowManyZeros {
 
     static Kattio io = new Kattio();
     public static void main(String[] args) {
-        
+        while (true) {
+            long m = io.getLong();
+            long n = io.getLong();
+            if (m == -1 && n == -1)
+                break;
+            io.println(countZeros(m, n));
+        }
+        io.close();
+    }
+
+    private static long countZeros(long m, long n) {
+        int zeros = 0;
+        return zeros;
     }
 }
