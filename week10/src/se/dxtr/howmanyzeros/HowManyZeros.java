311e5f9cfd1bc90aa2b062e33fff997d1c105909
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-11-27 14:18:45 +0100

    Howmanyzeros accepted, 2naire skeleton added.

--- week10/src/se/dxtr/howmanyzeros/HowManyZeros.java
+++ week10/src/se/dxtr/howmanyzeros/HowManyZeros.java
@@ -3,19 +3,50 @@ package se.dxtr.howmanyzeros;
 public class HowManyZeros {
 
     static Kattio io = new Kattio();
+
     public static void main(String[] args) {
         while (true) {
             long m = io.getLong();
             long n = io.getLong();
             if (m == -1 && n == -1)
                 break;
-            io.println(countZeros(m, n));
+            long excludeZeroCount = countZeros2(m - 1);
+            long totalZeroCount = countZeros2(n);
+            long intervalCount = totalZeroCount - excludeZeroCount;
+            if (m == 0)
+                intervalCount++;
+            io.println(intervalCount);
         }
         io.close();
     }
 
-    private static long countZeros(long m, long n) {
-        int zeros = 0;
+    private static long countZeros2(long n) {
+        long zeros = 0;
+        int pos = 1;
+
+        while (true) {
+            long div = n / pos;
+            long div10 = div / 10;
+
+            if (div10 == 0)
+                return zeros;
+
+            zeros += div % 10 == 0 ? (div10 - 1) * pos + (n % pos) + 1 : div10 * pos;
+            pos *= 10;
+        }
+    }
+
+    private static long countZerosNaive(long m, long n) {
+        long zeros = 0;
+
+        for (long i = m; i <= n; i++) {
+            char[] digits = String.valueOf(i).toCharArray();
+            for (int j = 0; j < digits.length; j++) {
+                if (digits[j] == '0') {
+                    zeros++;
+                }
+            }
+        }
         return zeros;
     }
 }
---
+++ week10/src/se/dxtr/twonaire/TwoNaire.java
@@ -0,0 +1,11 @@
+package se.dxtr.twonaire;
+
+/**
+ * Created by dexter on 27/11/15.
+ */
+public class TwoNaire {
+
+    public static void main(String[] args) {
+
+    }
+}
