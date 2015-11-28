6ac70c752e7cc50cffdfa37403bb86e1156135cb
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-11-28 15:57:32 +0100

    2naire accepted.

--- week10/src/se/dxtr/twonaire/TwoNaire.java
+++ week10/src/se/dxtr/twonaire/TwoNaire.java
@@ -1,11 +1,37 @@
 package se.dxtr.twonaire;
 
-/**
- * Created by dexter on 27/11/15.
- */
+import java.text.DecimalFormat;
+
 public class TwoNaire {
 
-    public static void main(String[] args) {
+    static Kattio io = new Kattio();
 
+    public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("0.000");
+        while (true) {
+            int questionCount = io.getInt();
+            if (questionCount == 0)
+                break;
+            double t = io.getDouble();
+            double expectedPrize = calculate(questionCount, t);
+            io.println(df.format(expectedPrize));
+        }
+        io.close();
+    }
+
+    private static double calculate(int questionCount, double t) {
+        double expected = Math.pow(2, questionCount);
+        for (int i = questionCount - 1; i >= 0; i--) {
+            double currentPrize = Math.pow(2, i);
+            double thresholdProbability = currentPrize / expected; // Smallest probability when it is worth answering the next question
+            if (thresholdProbability <= t) { // Always guess if threshold is smaller than t
+                expected = (t + 1) / 2 * expected;
+            } else { // Else calculate average values of stopping and playing
+                double stopValue = (thresholdProbability - t) / (1 - t) * currentPrize;
+                double playValue = (1 - thresholdProbability) / (1 - t) * ((1 + thresholdProbability) / 2) * expected;
+                expected = stopValue + playValue;
+            }
+        }
+        return expected;
     }
 }
